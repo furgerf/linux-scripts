31a3dff9ffeb8f11a1ad642f3a46a00a49af7d5f
mystyfly <mystyfly@gmail.com>
2014-11-01 15:19:03 +0000

    removed file

--- timetable_helper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-url="https://webapp.coventry.ac.uk/Timetable-main/Show/5977084?f=&t=&d=20140924&u=furgerf&k=CpWmMMiAjL9-ElWAas3Q7kB2\$Gw.X#view=month"
-
-wget -O timetable_data $url &> /dev/null
-
