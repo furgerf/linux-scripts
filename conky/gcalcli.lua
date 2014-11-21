d61b0b1b213e2412449077f84a7b5e23cc39a325
mystyfly <mystyfly@gmail.com>
2014-11-21 08:30:40 +0000

    small adjustments to conkyrc and clementine-dbus; changed gcalcli to do proper spacing with goto rather than monospace font

--- conky/gcalcli.lua
+++ conky/gcalcli.lua
@@ -9,9 +9,25 @@ while data ~= nil do
     if data:len() > 20 then
         data = data .. "\n"
     end
-    result = result .. data
+
+    local date = data:sub(1, 22)
+    local time
+    local title
+    local index = data:find("}", 22)
+
+    if index ~= nil then
+        time = data:sub(25, index + 7):gsub("  ", "")
+        title = data:sub(index + 10)
+    end
+
+    if time == nil then
+        result = result .. date
+    else
+        result = result .. date .. "${goto 66}" .. time .. "${goto 100}" .. title
+    end
+
     data = fh:read("*l")
 end
 
-print("${font DejaVuSansMono:size=8}" .. result:sub(1, #result - 1) .. "$font")
+print(result:sub(1, #result - 1))
 
