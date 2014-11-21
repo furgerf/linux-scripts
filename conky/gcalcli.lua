d48641a724187dfc1e72ee899ad3f162bbaa4b17
mystyfly <mystyfly@gmail.com>
2014-11-21 13:49:00 +0000

    added process stats to conky and fixed error in gcalcli output modification

--- conky/gcalcli.lua
+++ conky/gcalcli.lua
@@ -2,6 +2,9 @@
 
 local format = "%Y-%m-%dT%H:%M"
 local fh = io.popen("gcalcli --cal Privat#red --cal Arbeit#magenta --cal HSLU#yellow --cal Running#cyan --cal Squash#green --military --conky --color_date white agenda " .. os.date(format, os.time()) .. " " .. os.date(format, os.time() + 259200))
+
+--print(fh:read("*a"))
+
 local data = fh:read("*l")
 local result = ""
 
@@ -9,21 +12,21 @@ while data ~= nil do
     if data:len() > 20 then
         data = data .. "\n"
     end
-
-    local date = data:sub(1, 22)
+    
+    local date = data:sub(1, data:find("}"))
     local time
     local title
     local index = data:find("}", 22)
 
     if index ~= nil then
         time = data:sub(25, index + 7):gsub("  ", "")
+        if time:sub(1, 1) == "}" then
+            time = time:sub(2)
+        end
         title = data:sub(index + 10)
-    end
-
-    if time == nil then
-        result = result .. date
-    else
         result = result .. date .. "${goto 66}" .. time .. "${goto 100}" .. title
+    else
+        result = result .. date
     end
 
     data = fh:read("*l")
